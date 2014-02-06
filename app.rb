6ea6f2a6be43c96e75c2eadcff208b2c1e0669b9
Lautaro Orazi <orazile@gmail.com>
2014-02-05 22:07:43 -0300

    Refactor classes into Chist module

--- app.rb
+++ app.rb
@@ -8,9 +8,9 @@ require_relative "lib/chist"
 Cuba.use Rack::Session::Cookie, :secret => "ef9dfef977c094acfb5a642cdeb0f0be0258df5c1d58b8101aee0aae4e041ebedc02ba38d2b4a658"
 Cuba.use Rack::Protection
 
-Cuba.settings.merge! Settings.load
+Cuba.settings.merge! Chist::Settings.load
 
-DB = Sequel.connect DatabaseUtil.connection_path(Cuba.settings['db'])
+DB = Chist::Database.connect Cuba.settings['db']
 
 Dir["./lib/**/*.rb"].each        { |rb| require rb }
 Dir["./models/**/*.rb"].each     { |rb| require rb }
--- lib/chist.rb
+++ lib/chist.rb
@@ -1,19 +1,26 @@
 ENV['RACK_ENV'] ||= "development"
 require 'yaml'
 
-class Settings
-  def self.load(path_relative = "/../config/settings.yml")
-    begin
-      YAML.load_file(File.dirname(__FILE__) + path_relative)[ENV['RACK_ENV']]
-    rescue => e
-      puts e.message
-      abort("Can't load settings file")
+module Chist
+  class Settings
+    def self.load(path_relative = "/../config/settings.yml")
+      begin
+        YAML.load_file(File.dirname(__FILE__) + path_relative)[ENV['RACK_ENV']]
+      rescue => e
+        puts e.message
+        abort("Can't load settings file")
+      end
     end
   end
-end
 
-class DatabaseUtil
-  def self.connection_path(settings)
-    "postgres://#{settings['user']}:#{settings['password']}@#{settings['host']}:#{settings['port']}/#{settings['db_name']}"
+  class Database
+    def self.connect(settings)
+      Sequel.connect connection_path(settings)
+    end
+
+    private
+      def self.connection_path(settings)
+        "postgres://#{settings['user']}:#{settings['password']}@#{settings['host']}:#{settings['port']}/#{settings['db_name']}"
+      end
   end
 end
