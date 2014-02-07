c2bffe130a227aed1cf5b99f4b1e02cb11e842ef
Lautaro Orazi <orazile@gmail.com>
2014-02-07 14:07:09 -0300

    Add rack-test and users tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,18 @@ require 'minitest/autorun'
 require 'pp'
 require 'faker'
 require 'spawn'
+require 'rack/test'
 
 ENV["RACK_ENV"]  = "test"
 
 require File.dirname(__FILE__) + '/../app'
 
 require File.dirname(__FILE__) + '/spawners'
+
+class MiniTest::Spec
+  include Rack::Test::Methods
+
+  def app
+    Cuba.app
+  end
+end
