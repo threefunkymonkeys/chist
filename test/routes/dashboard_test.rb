f2785561d8a00df5aaf48f3bf8a49ba6b97311b8
Lautaro Orazi <orazile@gmail.com>
2014-02-10 10:45:30 -0300

    Add tests for login

---
+++ test/routes/dashboard_test.rb
@@ -0,0 +1,22 @@
+require File.expand_path(File.dirname(__FILE__)) + '/../test_helper'
+
+describe Chist::Dashboard do
+  def setup
+    User.all.each { |user| user.delete }
+    @user = User.spawn(password: 'qwe123')
+  end
+
+  it 'should login user' do
+    post '/users/login', {email: @user.email, password: 'qwe123'}
+
+    get '/dashboard'
+    assert_equal 200, last_response.status
+  end
+
+  it 'should not login user' do
+    post '/users/login', {email: @user.email, password: ''}
+
+    get '/dashboard'
+    assert_equal 302, last_response.status
+  end
+end
