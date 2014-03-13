99a78c1f8d13df33c967264ed8a7d380a1723857
Lautaro Orazi <orazile@gmail.com>
2014-03-13 08:08:37 -0300

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -3,7 +3,7 @@ Sequel.migration do
     create_table(:schema_info) do
       Integer :version, :default=>0, :null=>false
     end
-
+    
     create_table(:users) do
       primary_key :id
       String :email, :text=>true, :null=>false
@@ -11,6 +11,9 @@ Sequel.migration do
       String :name, :text=>true
       String :username, :text=>true
       TrueClass :valid, :default=>false
+      String :github_user, :text=>true
+      String :twitter_user, :text=>true
+      String :facebook_user, :text=>true
     end
   end
 end
