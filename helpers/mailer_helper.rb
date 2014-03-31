1b2fd54579786768c81f1b9e967ed0e8ed7be2b3
Lautaro Orazi <orazile@gmail.com>
2014-03-31 17:40:58 -0300

    Add mailer helper and activation email

---
+++ helpers/mailer_helper.rb
@@ -0,0 +1,23 @@
+require 'haml'
+
+module ChistApp::Helpers
+  class Mailer
+    def self.send_validation_code(user)
+      template = File.read('views/emails/account_validation.haml')
+      mail = Mail.new do
+        from     'no-reply@chist.com'
+        to       user.email
+        subject  I18n.t('emails.subject.validation')
+        body     ::Haml::Engine.new(template).render(Object.new, {user: user})
+      end
+
+      send(mail)
+    end
+
+    def self.send(mail)
+      puts mail.body
+      return unless ENV['RACK_ENV'] == :production
+      mail.deliver
+    end
+  end
+end
