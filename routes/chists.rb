6db3a4eb943cec1117453c5d20ad7b1d2d5b312b
Lautaro Orazi <orazile@gmail.com>
2014-04-15 09:05:59 -0300

    Add chist edit page

--- routes/chists.rb
+++ routes/chists.rb
@@ -13,6 +13,21 @@ module ChistApp
           not_found!
         end
 
+        on ':id/edit' do |chist_id|
+          on authenticated(User) do
+            chist = Chist[chist_id]
+            if chist && chist.user_id == current_user.id
+              res.write render("./views/layouts/app.haml") {
+                render("./views/chists/edit.haml", chist: chist, params: session.delete('chist.chist_params') || {})
+              }
+            else
+              not_found!
+            end
+          end
+
+          not_found!
+        end
+
         on ":id" do |chist_id|
           if chist = Chist[chist_id]
             res.write render("./views/layouts/app.haml", is_public: chist.public) {
@@ -21,6 +36,42 @@ module ChistApp
           else
             not_found!
           end
+        end
+
+        not_found!
+      end
+
+      on put do
+        on ':id/edit' do |chist_id|
+          on authenticated(User) do
+            chist = Chist[chist_id]
+            if chist && chist.user_id == current_user.id
+              begin
+                chist_params = req.params['chist']
+                chist_form = ChistApp::Validators::ChistForm.hatch(chist_params)
+
+                raise ArgumentError.new(chist_form.errors.full_messages.join(', ')) unless chist_form.valid?
+
+                chist.title = chist_params['title']
+                chist.chist_raw = chist_params['chist']
+                chist.chist = ChistApp::Parser.parse(chist_params['format'], chist_params['chist'])
+                chist.public = chist_params.has_key?('public')
+                chist.format = chist_params['format']
+                chist.save
+                flash[:success] = I18n.t('chists.chist_edited')
+                res.redirect "/chists/#{chist.id}"
+              rescue => e
+                flash[:error] = e.message
+                chist_params.delete('chist')
+                session['chist.chist_params'] = chist_params
+                res.redirect "/chists/#{chist.id}/edit"
+              end
+            else
+              not_found!
+            end
+          end
+
+          not_found!
         end
 
         not_found!
