eff2364e7e662fb51b67382a6d4ae5a2093c9d7b
Leonardo Mateo <leonardomateo@gmail.com>
2014-12-22 08:37:01 -0300

    Unifies Helpers module naming

--- helpers/environment_helper.rb
+++ helpers/environment_helper.rb
@@ -1,29 +1,27 @@
-module ChistApp
-  module Helpers
-    def self.init_environment(env)
-      self.set_env(env)
+module ChistApp::Helpers
+  def self.init_environment(env)
+    self.set_env(env)
 
-      db_params = {
-        'host' => ENV["DATABASE_HOST"],
-        'port' => ENV["DATABASE_PORT"],
-        'user' => ENV["DATABASE_USER"],
-        'password' => ENV["DATABASE_PASS"],
-        'db_name' => ENV["DATABASE_NAME"]
-      }
+    db_params = {
+      'host' => ENV["DATABASE_HOST"],
+      'port' => ENV["DATABASE_PORT"],
+      'user' => ENV["DATABASE_USER"],
+      'password' => ENV["DATABASE_PASS"],
+      'db_name' => ENV["DATABASE_NAME"]
+    }
 
-      ChistApp::Database.connect db_params
-    end
+    ChistApp::Database.connect db_params
+  end
 
-    def self.set_env(env)
-      filename = env.to_s + ".env.sh"
+  def self.set_env(env)
+    filename = env.to_s + ".env.sh"
 
-      if File.exists? filename
-        env_vars = File.read(filename)
-        env_vars.each_line do |var|
-          name, value = var.split("=")
-          if name && value
-            ENV[name.strip] = value.strip
-          end
+    if File.exists? filename
+      env_vars = File.read(filename)
+      env_vars.each_line do |var|
+        name, value = var.split("=")
+        if name && value
+          ENV[name.strip] = value.strip
         end
       end
     end
