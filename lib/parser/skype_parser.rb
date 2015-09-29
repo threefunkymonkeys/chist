da06783e767cf99ae8302d38f22d086210897966
Lautaro Orazi <orazile@gmail.com>
2015-09-29 12:28:06 -0300

    Fix skype parser

--- lib/parser/skype_parser.rb
+++ lib/parser/skype_parser.rb
@@ -3,7 +3,7 @@ class ChistApp::SkypeParser
   def self.parse(log)
     time_regexp = /^\[[^\]]+\]/
     #scan for participants
-    participants = log.scan(/\[\d+:\d{2}:\d{2}\s[AMPM]{2}\]\s([^:]+):.+/).uniq.collect! { |scan| scan.last }
+    participants = log.scan(/\[.*\d+:\d{2}:\d{2}\s[AMPM]{2}\]\s([^:]+):.+/).uniq.collect! { |scan| scan.last }
     #map classes for participants
     classes = Hash.new
     participants.each_with_index { |username, key| classes[username] = "<span class=\"username_#{key}\">#{username}</span>"}
