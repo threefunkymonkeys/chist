e10c3141051611ff4d00b4a9e885e79bc497641e
Lautaro Orazi <orazile@gmail.com>
2015-12-22 16:43:02 -0300

    Refactor to dashboard and reset password routes

--- routes/password.rb
+++ routes/password.rb
@@ -1,28 +1,26 @@
 class ChistApp::Password < Cuba
   define do
-    on 'reset' do
-      on authenticated(User) do
-        on get do
-          res.write render("./views/layouts/app.haml") {
-            render("./views/users/password_reset.haml")
-          }
-        end
-
-        on put do
-          begin
-            raise StandardError.new(I18n.t("user.password_doesnt_match")) unless req.params['new_password'] == req.params['confirm_password']
-            current_user.password = req.params['new_password']
-            current_user.update_password = false
-            current_user.save
-            flash[:success] = I18n.t('user.password_changed')
-            redirect! '/'
-          rescue StandardError => e
-            flash[:error] = e.message
-            redirect! '/password/reset'
-          end
-        end
-        not_found!
+    on authenticated(User) do
+      on get, 'reset' do
+        res.write render("./views/layouts/app.haml") {
+          render("./views/users/password_reset.haml")
+        }
       end
+
+      on put, 'reset' do
+        begin
+          raise StandardError.new(I18n.t("user.password_doesnt_match")) unless req.params['new_password'] == req.params['confirm_password']
+          current_user.password = req.params['new_password']
+          current_user.update_password = false
+          current_user.save
+          flash[:success] = I18n.t('user.password_changed')
+          redirect! '/'
+        rescue StandardError => e
+          flash[:error] = e.message
+          redirect! '/password/reset'
+        end
+      end
+
       not_found!
     end
 
