70a1f582a4e4b42aea286546482aeebf9e573762
Leonardo Mateo <leonardomateo@gmail.com>
2016-01-01 20:50:42 -0300

    Add utility representation methods to Chist

--- models/chist.rb
+++ models/chist.rb
@@ -19,4 +19,20 @@ class Chist < Sequel::Model
     self.class.db["DELETE FROM user_favorites WHERE user_id = ? AND chist_id = ?", self.user_id, self.id].all
     self.destroy
   end
+
+  def url
+    "/chists/#{id}"
+  end
+
+  def to_hash
+    {
+      :id         => id,
+      :title      => title,
+      :url        => url,
+      :public     => self.public,
+      :format     => format,
+      :created_at => created_at,
+      :updated_at => updated_at
+    }
+  end
 end
